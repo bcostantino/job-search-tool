733454c028ab694c90c0b1d40e73ed130d69c1fe
ben-c-2 <benchau99@gmail.com>
2020-12-30 18:59:10 -0500

    comments

--- scrape.py
+++ scrape.py
@@ -1,1 +1,13 @@
+###########################
+# ./job-search-tool       #
+# /scrape.py              #
+# @ author BCnBC          #
+# @ Date 2020-12-30       #
+# @ Last edit 2020-12-30  #
+#                         #
+#                         #
+# v1.0.0                  #
+#                         #
+###########################
+
 import config
