97cffcbaf536cf110acc5d98e96dc6c78f0bf90e
bcostantino <costan96@students.rowan.edu>
2020-12-30 18:41:42 -0500

    class init

--- config.py
+++ config.py
@@ -1,3 +1,19 @@
-## job-search-tool
-## config.py
-## v1.0.0
+####################################
+## ./job-search-tool               #
+## /config.py                      #
+##                                 #
+## @ author BCnBC                  #
+## @ date 2020-12-30               #
+##                                 #
+## v1.0.0                          #
+##                                 #
+##                                 #
+####################################
+
+
+# create site search class
+class site_search:
+    
+    # initialize variables
+    def __init__(self, url):
+        self.site_url = url
