a8e599e4c9ee1c3a68439a35ef67def5df5746c7
bcostantino <costan96@students.rowan.edu>
2020-12-30 19:20:59 -0500

    get site content

--- config.py
+++ config.py
@@ -13,6 +13,7 @@
 
 import requests
 from urllib.parse import urlparse
+from urllib.parse import urlencode
 from bs4 import BeautifulSoup
 
 # create site search class
@@ -30,9 +31,16 @@ class site_search:
         
         return formExists
         
-# create example url
-my_url = r'https://www.indeed.com/jobs?q={query}&l={where}&radius={rad}&jt={type}'
-query_vals = {'query':''}
+# create and format example url
+my_url = r'https://www.indeed.com/jobs?'
+query_vals = {'q':'Software Engineer',
+              'l':'Sicklerville, NJ 08081',
+              'radius': 50,
+              'jt':'fulltime'}
+
+my_url += urlencode(query_vals)
 
 # test class
 site1 = site_search(my_url)
+site1.is_site_form()
+print(site1.page.content)
