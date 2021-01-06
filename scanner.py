1b8b0e3adbfa889890ecd941a39bc5ee7f368b98
ben-c-2 <benchau99@gmail.com>
2021-01-06 13:38:07 -0500

    are these the changes you are looking for

--- scanner.py
+++ scanner.py
@@ -5,9 +5,20 @@ class scanner():
     def __init__(self):
         
         # scanner constructor
-        ql = input("job type")
-        sal = input("requested salary")
-        l = input("requested location")
-        rad = input("radius from location")
-        time = input("full time or part time")
+        cmd = input("Enter a command *type help for command list")
+        if cmd == "help"
+            print("crt_ scrape: creates a new scraper for user \n add_site: adds a new site for scraper to scrape \n add_param: adds parameter such as salaries/location/etc")
+        elif cmd == "crt_scrape"
+        #idk how to create the new scrape object
+        print("new scraper created")
+        elif cmd == "add_site"
+        #just tell me if this is what you want
+        print(site_name + "added")
+        elif cmd == "add_param" #can we have a param check to see if it is valid
+        print(param_name + "added")
+       # ql = input("job type")
+     #   sal = input("requested salary")
+      #  l = input("requested location")
+       # rad = input("radius from location")
+     #   time = input("full time or part time")
         pass
