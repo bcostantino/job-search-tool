450600ef568eeecf2663fc7da3b2d43905320405
ben-c-2 <benchau99@gmail.com>
2021-01-06 00:19:58 -0500

    messedup

--- config.py
+++ config.py
@@ -138,16 +138,7 @@ class site_search:
 ##
 ####
 
-class scanner():
-    def __init__(self):
-        
-        # scanner constructor
-        ql = input("job type")
-        sal = input("requested salary")
-        l = input("requested location")
-        rad = input("radius from location")
-        time = input("full time or part time")
-        pass
+
 
 def test_class():
     try:
