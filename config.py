36761bc1fdf214520138f5303e3aacafc6573f07
ben-c-2 <benchau99@gmail.com>
2021-01-05 23:43:47 -0500

    scan

--- config.py
+++ config.py
@@ -142,7 +142,11 @@ class scanner():
     def __init__(self):
         
         # scanner constructor
-        i = input("User input needed: \n ask to search for job in location, experience, relevency")
+        ql = input("job type")
+        sal = input("requested salary")
+        l = input("requested location")
+        rad = input("radius from location")
+        time = input("full time or part time")
         pass
 
 def test_class():
